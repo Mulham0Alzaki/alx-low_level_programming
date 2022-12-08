d58f3ac6f8be822c475d3e5a9eac4773c80b9f74
Mulham0Alzaki <mulhamalzaki@gmail.com>
2022-12-08 12:57:34 -0800

    5 print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>                                                                                                                                       
+
+                                                                                                                                                         
+
+/**                                                                                                                                                      
+ *
+ *  * main - Entry Point                                                                                                                                    
+ *
+ *   *                                                                                                                                                       
+ *
+ *    * Return: Always 0 (Sucsess)                                                                                                                            
+ *
+ *     */                                                                                                                                                      
+
+int main(void)                                                                                                                                           
+
+{                                                                                                                                                        
+
+	        printf"\"with proper grammar, but the outcome is a piece of art,");                                                                                    
+
+		        return (0);                                                                                                                                      
+
+			                                                                                                                                                         
+
+
